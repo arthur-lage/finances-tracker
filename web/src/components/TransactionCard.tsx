92b8fbbb4d36ba4614e6216b380cdc7719bcfe8c
Arthur Lage <arthurlage2006@gmail.com>
2023-05-06 21:00:07 -0300

    update card style and remove unnecessary code

--- web/src/components/TransactionCard.tsx
+++ web/src/components/TransactionCard.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useBalance } from "../hooks/useBalance";
 import { useTransactions } from "../hooks/useTransactions";
 import { ITransaction } from "../interfaces/ITransaction";
@@ -11,52 +11,35 @@ type TransactionCardType = {
 };
 
 export function TransactionCard({ transaction }: TransactionCardType) {
-  const { transactions, setTransactions } = useTransactions();
-  const { updateBalance } = useBalance();
-
-  async function handleDelete() {
-    try {
-      await transactionService.deleteTransactionById({
-        id: transaction.id,
-      });
-
-      const newTransactions = transactions.filter(
-        (currentTransaction) => currentTransaction.id != transaction.id
-      );
-
-      setTransactions(newTransactions);
-
-      await updateBalance();
-    } catch (err) {
-      console.error(err);
-    }
-  }
+  const navigate = useNavigate();
 
   return (
-    <Link
-      to={`/transactions/${transaction.id}`}
-      className={`flex flex-col text-black px-4 py-3 rounded-md ${
-        transaction.type === "INCOME" ? "bg-green-200" : "bg-red-200"
+    <div
+      className={`flex flex-col text-white px-4 py-3 rounded-md ${
+        transaction.type === "INCOME" ? "bg-emerald-600" : "bg-rose-600"
       }`}
     >
       <div className="flex flex-col gap-1 font-jost">
-        <strong className="font-bold">
+        <strong className="font-medium">
           {transaction.type === "INCOME" ? "+" : "-"}
           {BrazilianReal.format(transaction.value)}
         </strong>
         <p className="font-medium">{transaction.name}</p>
       </div>
 
-      <div className="w-full h-[1px] bg-zinc-900 my-4"></div>
+      <div className="w-full h-[1px] bg-white my-4"></div>
 
-      <div className="flex flex-col gap-1 font-semibold font-jost">
-        <p>{formatDate(transaction.date, "D/MM/YYYY")}</p>
-        <p>Added at {formatDate(transaction.createdAt, "D/MM/YYYY")}</p>
+      <div className="flex flex-col gap-1 font-semimedium font-jost">
+        <p>{formatDate(transaction.date, "DD/MM/YYYY")}</p>
+        <p>Added at {formatDate(transaction.createdAt, "DD/MM/YYYY")}</p>
       </div>
 
-      <button className="font-bold font-jost mt-5 bg-action-primary text-white py-2 rounded-md">
+      <button
+        onClick={() => navigate(`/transactions/${transaction.id}`)}
+        className="font-medium font-jost mt-5 bg-white text-black py-2 rounded-md"
+      >
         Details
       </button>
-    </Link>
+    </div>
   );
 }
